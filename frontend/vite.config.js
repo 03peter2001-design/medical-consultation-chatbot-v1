dcc80680cb2ff75f37099b3be2c341e0a6ae6bf4
mohw lab eec center <eec_center@lab.mohw.gov.tw>
2026-08-18 15:17:41 +0800

    chore(frontend): enable Vue development tools

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
 
 export default defineConfig({
   base: '/ai-consult/',
   publicDir: '../pic',
-  plugins: [vue()],
+  plugins: [vue(), vueDevTools()],
   server: {
     port: 5173,
   },
